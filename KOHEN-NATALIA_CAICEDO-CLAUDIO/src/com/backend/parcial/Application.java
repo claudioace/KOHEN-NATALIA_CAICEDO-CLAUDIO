f697a2309a52e035ab72c4cdf08af34fc56a509a
Natalia <kohen.natalia@gmail.com>
2023-10-26 21:10:29 -0300

    proyecto base1

--- KOHEN-NATALIA_CAICEDO-CLAUDIO/src/com/backend/parcial/Application.java
+++ KOHEN-NATALIA_CAICEDO-CLAUDIO/src/com/backend/parcial/Application.java
@@ -8,7 +8,7 @@ public class Application {
         Connection connection = null;
         try {
             Class.forName("org.h2.Driver");
-            connection = DriverManager.getConnection("jdbc:h2:~/nombrebasedatos;INIT=RUNSCRIPT FROM 'create.sql'", "sa", "sa");
+            connection = DriverManager.getConnection("jdbc:h2:~/odontologos;INIT=RUNSCRIPT FROM 'create.sql'", "sa", "sa");
 
         } catch (Exception e) {
             e.printStackTrace();
--- KOHEN-NATALIA_CAICEDO-CLAUDIO/src/com/backend/parcial/dao/H2Connection.java
+++ KOHEN-NATALIA_CAICEDO-CLAUDIO/src/com/backend/parcial/dao/H2Connection.java
@@ -7,6 +7,6 @@ import java.sql.SQLException;
 public class H2Connection {
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("org.h2.Driver");
-        return DriverManager.getConnection("jdbc:h2:~/nombrebasededatos", "sa", "sa");
+        return DriverManager.getConnection("jdbc:h2:~/odontologos", "sa", "sa");
     }
 }
