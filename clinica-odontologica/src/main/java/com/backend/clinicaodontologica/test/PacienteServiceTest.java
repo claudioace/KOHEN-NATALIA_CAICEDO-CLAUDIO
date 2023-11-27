02e87945d920eb6e1bfdb7dae40faad77582ed12
Natalia <kohen.natalia@gmail.com>
2023-11-27 17:50:20 -0300

    renderizando odontologos en js

--- clinica-odontologica/src/main/java/com/backend/clinicaodontologica/test/PacienteServiceTest.java
+++ clinica-odontologica/src/main/java/com/backend/clinicaodontologica/test/PacienteServiceTest.java
@@ -2,6 +2,7 @@ package com.backend.clinicaodontologica.test;
 
 
 import com.backend.clinicaodontologica.service.impl.PacienteService;
+import org.junit.Test;
 
 
 class PacienteServiceTest {
--- clinica-odontologica/src/main/resources/static/scripts/listarOdontologo.js
+++ clinica-odontologica/src/main/resources/static/scripts/listarOdontologo.js
@@ -1,79 +1,71 @@
 window.addEventListener("load", function () {
-  /* ---------------------- obtenemos variables globales ---------------------- */
-  const form = document.forms[0];
-  const odontologos = this.document.getElementById("listar");
-   const urlOdontologos = "https://localhost:8080/odontologos/listar";
-    
-  // console.log(listar);
+  window.addEventListener("load", function () {
+    /* ---------------------- obtenemos variables globales ---------------------- */
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    consultarPacientes();
-   
+    const form = document.forms[0];
+     const urlOdontologos = "http://localhost:8081/odontologos/listar";
+     const odontologoCreado = document.querySelector(".odontologos");
 
-    //Creamos el cuerpo de la request (petición al servidor)
-    function consultarPacientes() {
-      const settings = {
-        method: "GET",
-        headers: {
-          authorization: token,
-        },
-      };
-      console.log("consultando odontologos");
-      fetch(urlOdontologos, settings)
-        .then((response) => response.json())
-        .then((odontologo) => {
-          console.log("Estos son todos los odontologos");
-          console.log(odontologo);
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      consultarOdontologos();
+    })
 
-          renderizarOdontologos(odontologo);
-         
-        })
+      function consultarOdontologos() {
 
-        .catch((err) => {
-          console.warn("Promesa rechazada");
-          console.log(err);
-          if (err.status == 400) {
-            console.warn("id inválido");
-            alert("id inválido");
-          } else if (err.status == 401) {
-            console.warn("Requiere Autorización");
-            alert("Requiere Autorización");
-          } else if (err.status == 404) {
-            console.warn("Tarea inexistente");
-            alert("Tarea inexistente");
-          } else {
-            console.error("Error del servidor");
-            alert("Error del servidor");
-          }
+        const settings = {
+          method: "GET",
+        };
+        console.log("consultando odontologos");
+        fetch(urlOdontologos, settings)
+          .then((response) => response.json())
+          .then((odontologo) => {
+            console.log("Estos son todos los odontologos");
+            console.log(odontologo);
+            renderizarOdontologos(odontologo)
+
+          })
+
+          .catch((err) => {
+            console.warn("Promesa rechazada");
+            console.log(err);
+            if (err.status == 400) {
+              console.warn("id inválido");
+              alert("id inválido");
+            } else if (err.status == 401) {
+              console.warn("Requiere Autorización");
+              alert("Requiere Autorización");
+            } else if (err.status == 404) {
+              console.warn("odontologo inexistente");
+              alert("odontologo inexistente");
+            } else {
+              console.error("Error del servidor");
+              alert("Error del servidor");
+            }
+          });
+      }
+      function renderizarOdontologos(odontologo) {
+        console.log(odontologoCreado);
+        odontologoCreado.innerHTML = "";
+
+        const listaOdontologos = document.createElement("ul");
+
+        odontologo.forEach((odontolog) => {
+          const elementoOdontologo = document.createElement("li")
+          elementoOdontologo.classList.add('skeleton-container')
+          elementoOdontologo.innerHTML = `
+            <span >Nombre: ${odontolog.nombre}</span>
+            <span>Apellido: ${odontolog.apellido}</span>
+            <span>Número de Matrícula: ${odontolog.numeroMatricula}</span>`;
+
+          listaOdontologos.appendChild(elementoOdontologo);
         });
-    }
-    function renderizarOdontologos(odontologos) {
-      const odontologoCreado = document.querySelector(".odontologo-creado");
-      odontologoCreado .innerHTML = " ";
-      const cantOdontologos = document.getElementById("cantidad-odontologos");
-  
-      let contador = 0;
-      cantOdontologos.textContent = contador;
-  
-      odontologos.forEach((odontologo) => {
-          contador++;
-        odontologoCreado.innerHTML += `
-          <li class="odontologo">
-          <div class="creado">
-            <i class="fas fa-check-circle"></i> <!-- Cambiado de fa-regular fa-cicle-check a fas fa-check-circle -->
-          </div>
-          <div class="datosOdontologo">
-            <p class="nombre completo">${odontologo.nombre} ${odontologo.apellido} ${odontologo.matricula}</p> <!-- Cambiado de competo a completo -->
-          </div>
-        </li>`;
-    });
-    }
 
-  
-     
+        odontologoCreado.appendChild(listaOdontologos);
+      }
 
-});
 
-  });
+
+    })
+
 
